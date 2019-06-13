5e2e8152c06e5005b02e9f3a062d4f7b563cdc7a
Julius de Bruijn <bruijn@prisma.io>
2019-06-13 18:43:45 +0200

    TODO kind of working example with tower-web

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 #[macro_use]
-extern crate log;
+extern crate tower_web;
 
 #[macro_use]
 extern crate serde_derive;
 
-use faunadb::prelude::*;
-use futures::{
-    future,
-    stream::Stream,
-    sync::oneshot::{channel, Receiver},
-    Future,
-};
-use hyper::{header, service::service_fn, Body, Method, Request, Response, Server};
+#[macro_use]
+extern crate log;
+
+// mod blog; let's rethink this
+mod error;
+mod post;
+
+//use blog::Blog;
+use faunadb::client::{Client, ClientBuilder};
 use lazy_static::lazy_static;
-use std::{env, net::ToSocketAddrs, thread};
-use tokio::runtime::Runtime;
-use tokio_signal::unix::{Signal, SIGINT};
+use post::Post;
+use std::env;
+use tower_web::ServiceBuilder;
 
 lazy_static! {
     pub static ref FAUNA: Client = {
@@ -30,187 +31,11 @@ lazy_static! {
     };
 }
 
-type ResponseFuture = Box<Future<Item = Response<Body>, Error = Error> + Send + 'static>;
-
-#[derive(Debug)]
-enum Error {
-    Other,
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "ERROR HAPPENED")
-    }
-}
-
-impl std::error::Error for Error {
-    fn description(&self) -> &str {
-        "ERROR ERROR"
-    }
-
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        None
-    }
-}
-
-#[derive(Deserialize, Debug, Clone)]
-pub struct Post {
-    title: String,
-    tags: Vec<String>,
-}
-
-impl<'a> From<Post> for Object<'a> {
-    fn from(post: Post) -> Self {
-        let mut obj = Object::default();
-        obj.insert("title", post.title);
-        obj.insert("tags", Array::from(post.tags));
-
-        obj
-    }
-}
-
-pub struct Blog {
-    runtime: Runtime,
-}
-
-impl Blog {
-    pub fn new() -> Self {
-        Self {
-            runtime: Runtime::new().unwrap(),
-        }
-    }
-
-    pub fn run(&mut self, address: String, kill_switch: Receiver<()>) {
-        let mut addr_iter = address.to_socket_addrs().unwrap();
-        let addr = addr_iter.next().unwrap();
-
-        let server = Server::bind(&addr)
-            .serve(move || service_fn(move |req: Request<Body>| Self::service(req))); // No table service :ADAsgsrehawj
-
-        let _ = self.runtime.block_on(server.select2(kill_switch));
-    }
-
-    fn service<'a>(req: Request<Body>) -> ResponseFuture {
-        match (req.method(), req.uri().path()) {
-            (&Method::POST, "/posts") => {
-                let (_, body) = req.into_parts();
-
-                let future = body.concat2().and_then(move |body| {
-                    let post: Post = serde_json::from_slice(&body).unwrap();
-
-                    FAUNA
-                        .query(Create::new(Class::find("posts"), Object::from(post)))
-                        .then(|resp| match resp {
-                            Ok(result) => {
-                                let resource = result.resource.as_object().unwrap();
-                                let reference = resource
-                                    .get("ref")
-                                    .and_then(|res| res.as_reference())
-                                    .unwrap();
-
-                                let location =
-                                    format!("http://localhost:4200/posts/{}", reference.id);
-
-                                let mut builder = Response::builder();
-                                builder.header(header::LOCATION, location);
-                                builder.status(201);
-
-                                Ok(builder.body(Body::empty()).unwrap())
-                            }
-                            Err(e) => {
-                                error!("Error with Fauna: {:?}", e);
-
-                                let mut builder = Response::builder();
-                                builder.status(500);
-
-                                Ok(builder.body(Body::from("INTERNAL SERVER ERROR")).unwrap())
-                            }
-                        })
-                });
-
-                Box::new(future.map_err(|_| Error::Other))
-            }
-            _ => {
-                let body = Body::from("Hello, world!");
-                let res = Response::builder().status(200).body(body);
-
-                Box::new(future::ok(res.unwrap()))
-            }
-        }
-    }
-
-    fn delete_database(&mut self) {
-        self.run_expr(Delete::new(Index::find("posts_by_title")));
-        self.run_expr(Delete::new(Index::find("posts_by_tags_with_title")));
-        self.delete_class("posts");
-    }
-
-    fn run_expr<'a>(&mut self, expr: impl Into<Expr<'a>>) {
-        self.runtime.block_on(FAUNA.query(expr)).unwrap();
-    }
-
-    fn create_class(&mut self, name: &str) {
-        trace!("Create class {}.", name);
-        self.run_expr(CreateClass::new(ClassParams::new(name)));
-
-        self.run_expr(CreateIndex::new(IndexParams::new(
-            format!("all_{}", name),
-            Class::find(name),
-        )));
-    }
-
-    fn delete_class(&mut self, name: &str) {
-        trace!("Delete class {}.", name);
-        self.run_expr(Delete::new(Index::find(format!("all_{}", name))));
-        self.run_expr(Delete::new(Index::find(format!("schema_{}", name))));
-        self.run_expr(Delete::new(Class::find(name)));
-    }
-
-    fn create_schema(&mut self) {
-        self.create_class("posts");
-
-        {
-            let mut params = IndexParams::new("posts_by_title", Class::find("posts"));
-            params.terms(vec![Term::field(vec!["data", "title"])]);
-            self.run_expr(CreateIndex::new(params));
-
-            let mut params = IndexParams::new("posts_by_tags_with_title", Class::find("posts"));
-            params.terms(vec![Term::field(vec!["data", "tags"])]);
-            params.values(vec![IndexValue::field(vec!["data", "title"])]);
-            self.run_expr(CreateIndex::new(params));
-        }
-    }
-}
-
 fn main() {
     pretty_env_logger::init();
 
-    let mut blog = Blog::new();
+    let addr = "127.0.0.1:8080".parse().expect("Invalid address");
+    info!("Listening on http://{}", addr);
 
-    match env::var("MIGRATE") {
-        Ok(ref cmd) if cmd == "delete" => blog.delete_database(),
-        Ok(_) => blog.create_schema(),
-        _ => {
-            let (tx, rx) = channel();
-
-            let server = thread::spawn(move || {
-                info!("Server listening on port 4200");
-                blog.run(format!("0.0.0.0:4200"), rx);
-                info!("Server going down...");
-            });
-
-            let _ = Signal::new(SIGINT)
-                .flatten_stream()
-                .into_future()
-                .and_then(|_| {
-                    if let Err(error) = tx.send(()) {
-                        error!("Error sending the shutdown signal {:?}", error);
-                    };
-
-                    server.join().unwrap();
-                    Ok(())
-                })
-                .wait();
-        }
-    }
+    ServiceBuilder::new().resource(Post).run(&addr).unwrap()
 }
---
+++ src/post.rs
@@ -0,0 +1,98 @@
+use crate::{error::Error, FAUNA};
+use faunadb::{error::Error as FaunaError, prelude::*};
+use futures::future::Future;
+use std::convert::TryFrom;
+
+pub struct Post;
+
+#[derive(Extract, Response, Debug, Clone)]
+#[web(header(name = "content-type", value = "application/json"))]
+pub struct PostData {
+    title: String,
+    tags: Vec<String>,
+}
+
+#[derive(Debug, Response)]
+#[web(status = "201")]
+pub struct PostCreated {
+    #[web(header)]
+    location: String,
+}
+
+impl_web! {
+    impl Post {
+        #[post("/posts")]
+        #[content_type("application/json")]
+        fn create(&self, body: PostData) -> impl Future<Item = PostCreated, Error = FaunaError> + Send {
+            FAUNA
+                .query(Create::new(Class::find("posts"), Object::from(body)))
+                .map(|resp| {
+                    let resource = resp.resource;
+                    let reference = resource.get_reference().unwrap();
+
+                    PostCreated {
+                        location: format!("http://localhost:8080/posts/{}", reference.id),
+                    }
+                })
+        }
+
+        #[get("/posts/:id")]
+        #[content_type("application/json")]
+        fn find(&self, id: String) -> impl Future<Item = PostData, Error = FaunaError> + Send {
+            let mut reference = Ref::instance(id);
+            reference.set_class("posts");
+
+            FAUNA
+                .query(Get::instance(reference))
+                .map_err(|e| {
+                    dbg!(&e);
+                    e
+                })
+                .map(|resp| {
+                    let mut resource = resp.resource.into_object().unwrap();
+                    let mut data = resource.remove("data").unwrap().into_object().unwrap();
+                    let tags = data.remove("tags").unwrap();
+
+                    PostData {
+                        title: data.remove("title").and_then(|r| r.into_string()).unwrap(),
+                        tags: tags.map_array(|e| e.into_string()).unwrap(),
+                    }
+                })
+        }
+    }
+}
+
+impl<'a> From<PostData> for Object<'a> {
+    fn from(post: PostData) -> Self {
+        let mut obj = Object::default();
+        obj.insert("title", post.title);
+        obj.insert("tags", Array::from(post.tags));
+
+        obj
+    }
+}
+
+impl TryFrom<Value> for PostData {
+    type Error = Error;
+
+    fn try_from(value: Value) -> Result<PostData, Error> {
+        fn lift<T>(opt: Option<T>) -> Result<T, Error> {
+            opt.ok_or(Error::Conversion)
+        }
+
+        let mut obj = lift(value.into_object())?;
+        let mut data = lift(lift(obj.remove("data"))?.into_object())?;
+
+        let tag_values = lift(data.remove("tags"))?.into_array();
+
+        let tags: Result<Vec<String>, Error> = lift(tag_values)?
+            .into_iter()
+            .map(|val| lift(val.into_string()))
+            .collect();
+
+        let tags = tags?;
+        let title = lift(lift(data.remove("title"))?.into_string())?;
+
+        Ok(PostData { title, tags })
+    }
+}
